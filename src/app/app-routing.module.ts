de1c169a15ae34dbcc0a488c4def5f0aae0a076b
Annabelle-anna <inezannabelle@gmail.com>
2020-12-30 08:08:31 +0200

    my project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component:HomeComponent
+  },
+  {
+    path: 'nav', component:NavbarComponent
+  },
+  {
+    path:'footer', component:FooterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
